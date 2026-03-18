7ec5e1fb758806e4e4a55d31757897f34a9a84e5
Giarno Divino <giar@Giarnos-MacBook-Air.local>
2026-03-18 19:33:40 +0800

    Fix basename for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: process.env.GITHUB_ACTIONS ? "/jnfv3/" : "/",
+  base: "/",
 }));
